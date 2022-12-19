52306693aec4cfdcdfa7aa86b48d092fc64a225c
Cassian Nitu <nitu.cassian@gmail.com>
2022-12-19 17:59:47 +0200

    better message

--- src/main/java/com/example/demo/advice/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/advice/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.demo.advice.exception;
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String name, Long id) {
-        super("Could not find resource with type: "+ name +" and id: "+ id);
+        super("Could not find resource:"+ name +" with id:"+ id);
     }
 
 }
